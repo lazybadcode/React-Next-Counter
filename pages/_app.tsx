d122321e5a1138d020b39334e5e986e5c91ad49d
lazybadcode <lazybadcode@gmail.com>
2022-11-14 20:14:26 +0700

    Revert "react lists button for counting"
    This reverts commit 36df453e1bcc4a4bd037b764c4b99372bfd9a2ab.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
-export default function App({ Component, pageProps }: any) {
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
